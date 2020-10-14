4b03838ef9cf674c5e89ba909fe6b75c79a2cc6b
heysivani <heysivani@gmail.com>
2020-10-14 11:59:45 +0900

    schema structure complete
    Co-authored-by: heysivani <heysivani@users.noreply.github.com>
    Co-authored-by: alexdang1993374 <alexdang1993374@users.noreply.github.com>

--- server/schema.js
+++ server/schema.js
@@ -4,9 +4,53 @@ const typeDefs = gql`
   # Your schema goes here.
   # The schema should model the full data object available!
 
+  type weight {
+    minimum: String
+    maximum: String
+  }
+
+  type height {
+    minimum: String
+    maximum: String
+  }
+
+  type evolutionRequirements {
+    amount: Int
+    name: String
+  }
+
+  type evolutions {
+    id: String
+    name: String
+  }
+
+  type attackTypes {
+    fast: [attack]
+    special: [attack]
+  }
+
+  type attack {
+    name: String
+    type: String
+    damage: Int
+  }
+
   type Pokemon {
     id: String
     name: String!
+    classification: String
+    types: [String]
+    resistant: [String]
+    weaknesses: [String]
+    weight: weight
+    height: height
+    fleeRate: Float
+    previousEvolutions: [evolutions]
+    evolutionRequirements: evolutionRequirements
+    evolutions: [evolutions]
+    maxCP: Int
+    maxHP: Int
+    attacks: attackTypes
   }
   type Query {
     Pokemons: [Pokemon]
